d3b52a95e7e1524fab9e9bd5fb6230cfdbf9d960
Pierre Raybaut <p.raybaut@codra.fr>
2023-07-26 19:10:34 +0200

    Tests: added guidata test launcher

---
+++ plotpy/tests/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the terms of the BSD 3-Clause
+# (see plotpy/LICENSE for details)
+
+"""
+plotpy test package
+===================
+"""
+
+
+def run():
+    """Run plotpy test launcher"""
+    from guidata.guitest import run_testlauncher
+
+    import plotpy
+
+    run_testlauncher(plotpy)
+
+
+if __name__ == "__main__":
+    run()
