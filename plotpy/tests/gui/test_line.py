8715bf19aa47f93e3537b33048fe40d2fd16b808
Pierre Raybaut <p.raybaut@codra.fr>
2023-07-28 10:20:17 +0200

    test_line.py: fixed type mismatch

--- plotpy/tests/gui/test_line.py
+++ plotpy/tests/gui/test_line.py
@@ -5,21 +5,18 @@
 
 """Internal test related to pcolor feature"""
 
-
 import numpy as np
 import pytest
 
 from plotpy._scaler import _line_test as line
 
 N = 10
-imin = np.full((N,), N, dtype=int)
-imax = np.full((N,), 0, dtype=int)
-
-test_line_data = [(0, 0, 9, 9), (9, 9, 0, 0), (0, 5, 0, 9), (2, 5, 7, 5), (0, 1, 2, 9)]
-test_tri_data = [(0, 1, 2, 9, 8, 7)]
+TEST_LINE_DATA = [(0, 0, 9, 9), (9, 9, 0, 0), (0, 5, 0, 9), (2, 5, 7, 5), (0, 1, 2, 9)]
+TEST_TRI_DATA = [(0, 1, 2, 9, 8, 7)]
 
 
 def print_tri(imin, imax):
+    """Print triangle"""
     for i in range(N):
         for j in range(N):
             if j < imin[i] or j > imax[i]:
@@ -29,20 +26,24 @@ def print_tri(imin, imax):
         print()
 
 
-@pytest.mark.parametrize("x0,y0,x1,y1", test_line_data)
+@pytest.mark.parametrize("x0,y0,x1,y1", TEST_LINE_DATA)
 def test_line(x0, y0, x1, y1):
+    """Test line"""
     print(x0, ",", y0, "->", x1, ",", y1)
-    imin = np.full((N,), N, dtype=int)
-    imax = np.full((N,), 0, dtype=int)
+    imin = np.full((N,), N, dtype=np.int32)
+    imax = np.full((N,), 0, dtype=np.int32)
     print("imin:", repr(imin), imin.dtype)
     print("imax:", repr(imax), imax.dtype)
     line(x0, y0, x1, y1, N, imin, imax)
     print_tri(imin, imax)
 
 
-@pytest.mark.parametrize("x0,y0,x1,y1,x2,y2", test_tri_data)
+@pytest.mark.parametrize("x0,y0,x1,y1,x2,y2", TEST_TRI_DATA)
 def test_tri(x0, y0, x1, y1, x2, y2):
+    """Test triangle"""
     print(x0, ",", y0, "->", x1, ",", y1)
+    imin = np.full((N,), N, dtype=np.int32)
+    imax = np.full((N,), 0, dtype=np.int32)
     imin[:] = N + 1
     imax[:] = -1
     line(x0, y0, x1, y1, N, imin, imax)
