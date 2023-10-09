149e7a0f4eb1f63f7263dc4e52148f4f12080dea
Pierre Raybaut <p.raybaut@codra.fr>
2023-10-09 11:58:05 +0200

    VSCode tasks (Windows): support for multiple envs

--- scripts/build_inplace.bat
+++ scripts/build_inplace.bat
@@ -7,11 +7,22 @@ REM Licensed under the terms of the MIT License
 REM Copyright (c) 2020 Pierre Raybaut
 REM (see PythonQwt LICENSE file for more details)
 REM ======================================================
+setlocal enabledelayedexpansion
 call %~dp0utils GetScriptPath SCRIPTPATH
-call %FUNC% GetLibName LIBNAME
 call %FUNC% GetModName MODNAME
 call %FUNC% SetPythonPath
-call %FUNC% UsePython
+
 if exist MANIFEST ( del /q MANIFEST )
-python setup.py build_ext --inplace
+:: Iterate over all directories in the grandparent directory
+:: (WinPython base directories)
+call %FUNC% GetPythonExeGrandParentDir DIR0
+for /D %%d in ("%DIR0%*") do (
+    set WINPYDIRBASE=%%d
+    call !WINPYDIRBASE!\scripts\env.bat
+    echo ******************************************************************************
+    echo Building %MODNAME% from "%%d"
+    echo ******************************************************************************
+    python setup.py build_ext --inplace
+    echo ----
+)
 call %FUNC% EndOfScript
