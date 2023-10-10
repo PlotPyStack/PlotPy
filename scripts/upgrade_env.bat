3f04af4e38e02c3975f98f8068b7de373c090ca3
Pierre Raybaut <p.raybaut@codra.fr>
2023-10-10 10:35:37 +0200

    upgrade_env.bat: upgrading all envs

--- scripts/upgrade_env.bat
+++ scripts/upgrade_env.bat
@@ -10,6 +10,17 @@ REM ======================================================
 setlocal
 call %~dp0utils GetScriptPath SCRIPTPATH
 cd %SCRIPTPATH%\..
-%PYTHON% -m pip install --upgrade -r dev\requirements.txt
+
+:: Iterate over all directories in the grandparent directory
+:: (WinPython base directories)
+call %FUNC% GetPythonExeGrandParentDir DIR0
+for /D %%d in ("%DIR0%*") do (
+    set WINPYDIRBASE=%%d
+    call !WINPYDIRBASE!\scripts\env.bat
+    echo Upgrading environment for "%%d":
+    python -m pip install --upgrade -r dev\requirements.txt
+    echo ----
+)
+
 %PYTHON% -m pip list > dev\pip_list.txt
 call %FUNC% EndOfScript
