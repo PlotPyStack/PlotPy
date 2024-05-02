efeb57a9c666987a26b9fe19ad9f539a7029a8b1
Pierre Raybaut <p.raybaut@codra.fr>
2024-05-02 10:11:24 +0200

    High DPI curve plot test: set env var in func

--- plotpy/tests/items/test_curves_highdpi.py
+++ plotpy/tests/items/test_curves_highdpi.py
@@ -16,15 +16,16 @@ from guidata.qthelpers import qt_app_context
 from plotpy.builder import make
 from plotpy.tests import vistools as ptv
 
-# Performance should be the same with "1" and "2" scale factors:
-# (as of today, this is not the case, but it has to be fixed in the future:
-#  https://github.com/PlotPyStack/PlotPy/issues/10)
-os.environ["QT_SCALE_FACTOR"] = "2"
-
 
 @pytest.mark.skip(reason="This test is not relevant for the automated test suite")
 def test_plot_highdpi():
     """Curve plotting test with high DPI"""
+
+    # Performance should be the same with "1" and "2" scale factors:
+    # (as of today, this is not the case, but it has to be fixed in the future:
+    #  https://github.com/PlotPyStack/PlotPy/issues/10)
+    os.environ["QT_SCALE_FACTOR"] = "2"
+
     x = np.linspace(-10, 10, 5000000)  # 5M points are needed to see the difference
     y = np.sin(np.sin(np.sin(x)))
     with qt_app_context(exec_loop=True):
