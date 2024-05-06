f84224ea16625e0cf9808bf0f751ddef3c53919f
Pierre Raybaut <p.raybaut@codra.fr>
2024-05-06 17:52:10 +0200

    Updated unit test associated to #10

--- plotpy/tests/items/test_curves_highdpi.py
+++ plotpy/tests/items/test_curves_highdpi.py
@@ -21,16 +21,25 @@ from plotpy.tests import vistools as ptv
 def test_plot_highdpi():
     """Curve plotting test with high DPI"""
 
-    # Performance should be the same with "1" and "2" scale factors:
-    # (as of today, this is not the case, but it has to be fixed in the future:
-    #  https://github.com/PlotPyStack/PlotPy/issues/10)
+    # When setting the QT_SCALE_FACTOR to "2", performance is degraded, due to the
+    # increased number of points to be drawn. As a workaround, we use the downsampling
+    # feature to reduce the number of points to be drawn.
+    # We also enable the antialiasing feature to improve the rendering of the curves,
+    # which is also affected by the high DPI setting.
+    # See https://github.com/PlotPyStack/PlotPy/issues/10
     os.environ["QT_SCALE_FACTOR"] = "2"
 
-    x = np.linspace(-10, 10, 5000000)  # 5M points are needed to see the difference
+    npoints = 5000000  # 5M points are needed to see the difference
+    dsamp_factor = npoints // 50000  # Max 50000 points to be drawn
+    x = np.linspace(-10, 10, npoints)
     y = np.sin(np.sin(np.sin(x)))
     with qt_app_context(exec_loop=True):
         _win = ptv.show_items(
-            [make.curve(x, y, color="b"), make.legend("TR")],
+            [
+                make.curve(x, y, color="b", dsamp_factor=dsamp_factor, use_dsamp=True),
+                make.legend("TR"),
+            ],
+            curve_antialiasing=True,
             wintitle=test_plot_highdpi.__doc__,
             title="Curves with high DPI",
             plot_type="curve",
