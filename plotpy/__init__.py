2d9124e025a5a14d576221dc797e77fe36ed6977
Pierre Raybaut <p.raybaut@codra.fr>
2026-04-22 09:49:58 +0200

    bump version to 2.9.1

--- plotpy/__init__.py
+++ plotpy/__init__.py
@@ -20,7 +20,7 @@ External resources:
 .. _GitHub: https://github.com/PierreRaybaut/plotpy
 """
 
-__version__ = "2.9.0"
+__version__ = "2.9.1"
 __VERSION__ = tuple([int(number) for number in __version__.split(".")])
 
 # --- Important note: DATAPATH and LOCALEPATH are used by guidata.configtools
